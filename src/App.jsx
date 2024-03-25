20d76112491945c11adda96eb29879a06aa13b16
sumit-kr-das <sumitkrdas777@gmail.com>
2024-03-25 13:00:18 +0530

    react lazy added

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,176 @@
 import { Toaster } from "react-hot-toast";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import {
-  Account,
-  AddHotel,
-  AddRoom,
-  Blog,
-  BlogDetails,
-  Bookings,
-  Destination,
-  Error,
-  Home,
-  Hotel,
-  HotelList,
-  HotelType,
-  Layout,
-  ListHotel,
-  ListRooms,
-  Login,
-  Register,
-} from "./Pages";
 import PrivateRoute from "./privateRoutes/PrivateRoute";
+import Loader from "./components/Loader/Loader";
+import { Suspense, lazy } from "react";
+
+// --------------------------- pages ---------------------------
+const Home = lazy(() => import("./Pages/Home"));
+const Hotel = lazy(() => import("./Pages/Hotel"));
+const HotelList = lazy(() => import("./Pages/HotelList"));
+const Destination = lazy(() => import("./Pages/Destination"));
+const HotelType = lazy(() => import("./Pages/HotelType"));
+const Login = lazy(() => import("./Pages/Login"));
+const Register = lazy(() => import("./Pages/Register"));
+const Blog = lazy(() => import("./Pages/Blog"));
+const BlogDetails = lazy(() => import("./Pages/BlogDetails"));
+const Account = lazy(() => import("./Pages/Account"));
+const Bookings = lazy(() => import("./Pages/Bookings"));
+const ListHotel = lazy(() => import("./Pages/private/hotel/ListHotel"));
+const AddHotel = lazy(() => import("./Pages/private/hotel/AddHotel"));
+const AddRoom = lazy(() => import("./Pages/private/hotel/AddRoom"));
+const ListRooms = lazy(() => import("./Pages/private/hotel/ListRooms"));
+const Error = lazy(() => import("./Pages/Error"));
+const Layout = lazy(() => import("./Pages/private/hotel/layout/Layout"));
 
 function App() {
   return (
     <Router>
       <Toaster position="top-right" reverseOrder={false} />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/hotels" element={<Hotel />} />
-        <Route path="/hotel/:id" element={<HotelList />} />
-        <Route path="/find/:destination" element={<Destination />} />
-        <Route path="/type/:type/:count" element={<HotelType />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/blog/:id" element={<BlogDetails />} />
-        <Route path="/account" element={<Account />} />
-        <Route path="/bookings" element={<Bookings />} />
+        <Route
+          path="/"
+          element={
+            <Suspense fallback={<Loader />}>
+              <Home />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/hotels"
+          element={
+            <Suspense fallback={<Loader />}>
+              <Hotel />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/hotel/:id"
+          element={
+            <Suspense fallback={<Loader />}>
+              <HotelList />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/find/:destination"
+          element={
+            <Suspense fallback={<Loader />}>
+              <Destination />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/type/:type/:count"
+          element={
+            <Suspense fallback={<Loader />}>
+              <HotelType />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <Suspense fallback={<Loader />}>
+              <Login />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <Suspense fallback={<Loader />}>
+              <Register />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/blog"
+          element={
+            <Suspense fallback={<Loader />}>
+              <Blog />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/blog/:id"
+          element={
+            <Suspense fallback={<Loader />}>
+              <BlogDetails />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/account"
+          element={
+            <Suspense fallback={<Loader />}>
+              <Account />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/bookings"
+          element={
+            <Suspense fallback={<Loader />}>
+              <Bookings />
+            </Suspense>
+          }
+        />
         {/* Admin page */}
         <Route
           path="/dashboard/hotel"
-          element={<PrivateRoute element={<Layout />} roles={["hotel"]} />}
+          element={
+            <PrivateRoute
+              element={
+                <Suspense fallback={<Loader />}>
+                  <Layout />
+                </Suspense>
+              }
+              roles={["hotel"]}
+            />
+          }
         >
-          <Route path="/dashboard/hotel" element={<ListHotel />} />
-          <Route path="/dashboard/hotel/add-hotel" element={<AddHotel />} />
-          <Route path="/dashboard/hotel/add-room" element={<AddRoom />} />
-          <Route path="/dashboard/hotel/list-room" element={<ListRooms />} />
+          <Route
+            path="/dashboard/hotel"
+            element={
+              <Suspense fallback={<Loader />}>
+                <ListHotel />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/dashboard/hotel/add-hotel"
+            element={
+              <Suspense fallback={<Loader />}>
+                <AddHotel />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/dashboard/hotel/add-room"
+            element={
+              <Suspense fallback={<Loader />}>
+                <AddRoom />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/dashboard/hotel/list-room"
+            element={
+              <Suspense fallback={<Loader />}>
+                <ListRooms />
+              </Suspense>
+            }
+          />
         </Route>
-        <Route path="/*" element={<Error />} />
+        <Route
+          path="/*"
+          element={
+            <Suspense fallback={<Loader />}>
+              <Error />
+            </Suspense>
+          }
+        />
       </Routes>
     </Router>
   );
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,24 @@
+import "./loader.scss";
+
+const Loader = () => {
+  return (
+    <section className="loader_body">
+      <div className="holder">
+        <div className="preloader">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Loader;
