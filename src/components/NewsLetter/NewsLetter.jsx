fe40422d3604995429b0cac431c0212d0badb341
sumit-kr-das <sumitkrdas777@gmail.com>
2024-05-20 11:32:38 +0530

    container added in home components

--- src/components/NewsLetter/NewsLetter.jsx
+++ src/components/NewsLetter/NewsLetter.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useState } from "react";
 import toast from "react-hot-toast";
 import "./newsLetter.scss";
+import Container from "../../layout/container/Container";
 
 const NewsLetter = () => {
   const [emailID, setEmail] = useState("");
@@ -24,40 +25,42 @@ const NewsLetter = () => {
     setEmail("");
   };
   return (
-    <section className="news_letter">
-      <div className="news_content">
-        <p className="news_heading">Join our newsletter 🎉</p>
-        <p className="news_para">
-          Read and share new perspectives on just about any topic. Everyone’s
-          welcome.
-        </p>
-        <div className="news_content_options">
-          <div className="news_content_options_card">
-            <div className="first_option_card">01</div>
-            <p>Get more discount</p>
+    <Container>
+      <section className="news_letter">
+        <div className="news_content">
+          <p className="news_heading">Join our newsletter 🎉</p>
+          <p className="news_para">
+            Read and share new perspectives on just about any topic. Everyone’s
+            welcome.
+          </p>
+          <div className="news_content_options">
+            <div className="news_content_options_card">
+              <div className="first_option_card">01</div>
+              <p>Get more discount</p>
+            </div>
+            <div className="news_content_options_card">
+              <div className="second_option_card">02</div>
+              <p>Get premium magazines</p>
+            </div>
           </div>
-          <div className="news_content_options_card">
-            <div className="second_option_card">02</div>
-            <p>Get premium magazines</p>
+          <div className="input_container">
+            <input
+              type="email"
+              placeholder="Enter your email"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+            <button className="btn_primary" onClick={handleSubmit}>
+              Submit
+            </button>
           </div>
         </div>
-        <div className="input_container">
-          <input
-            type="email"
-            placeholder="Enter your email"
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
-          />
-          <button className="btn_primary" onClick={handleSubmit}>
-            Submit
-          </button>
+        <div className="news_img">
+          <img src="/assets/newsletter.png" alt="newsletter_img" />
         </div>
-      </div>
-      <div className="news_img">
-        <img src="/assets/newsletter.png" alt="newsletter_img" />
-      </div>
-    </section>
+      </section>
+    </Container>
   );
 };
 
